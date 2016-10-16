02d8d3e17aecdc4e240e9be43dd56c08e4bc42a3
yunovichso <syunovich@codereign.net>
2016-10-16 10:59:47 +0300

    initial client api

---
+++ Client/services/api/httpTransport.js
@@ -0,0 +1,57 @@
+class HttpTransport {
+    constructor(defaultOptions){
+        this.defaultOptions = defaultOptions;
+    }
+
+    get(url, params, options){
+        options = {...this.defaultOptions, ...options,
+            method: 'GET'
+        };
+
+
+        url = this._addParamsToUrl(url, params);
+
+        alert(url);
+
+        return fetch(url, options).then(resp=>{
+            //alert(resp.text());
+            return resp.json();
+        });
+    }
+
+    post(url, data, options){
+        options = {...this.defaultOptions, ...options,
+            body: data,
+            method: 'POST'
+        };
+
+        return fetch(url, options).then(resp=>{
+            return resp.json();
+        });
+    }
+
+    _addParamsToUrl(url, paramsObj){
+        const paramsString = this._escapedParams(paramsObj);
+
+        if (!paramsString) return url;
+
+        if (url.indexOf('?')){
+            url += '&' + paramsString;
+        }else{
+            url += '?' + paramsString;
+        }
+
+        return url;
+    }
+
+    _escapedParams(params){
+        var arr = []
+        for(var key in params){
+            arr.push(`${key}=${encodeURIComponent(params[key])}`);
+        }
+
+        return arr.join('&');
+    }
+}
+
+export default HttpTransport;
