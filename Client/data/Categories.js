e82fb9e2259d6404ee4cda60288440ac53883b2f
Ivan <ivun@wirebyte.com>
2016-10-16 11:08:31 +0300

    Added initial static data

---
+++ Client/data/Categories.js
@@ -0,0 +1,22 @@
+export default data = [
+    {
+        "title": "Pizza",
+        "price": "from $5.95",
+    },
+    {
+        "title": "Pasta",
+        "price": "from $8.95",
+    },
+    {
+        "title": "Grill",
+        "price": "from $10.00",
+    },
+    {
+        "title": "Beverages: alcoholic",
+        "price": "from $3.95",
+    },
+    {
+        "title": "Beverages: non-alcoholic",
+        "price": "from $0.95",
+    }
+];
---
+++ Client/data/Restaurants.js
@@ -0,0 +1,58 @@
+export default data = [
+    {
+        "title": "HurryFox Pizza",
+        "subtitle": "$, pizza, fastfood",
+        "picture": {
+            "thumbnail": "http://www.burrivery.com/assets/restraurants/",
+        },
+    },
+    {
+        "title": "МакДак",
+        "subtitle": "$$, fastfood, burgers",
+        "picture": {
+            "thumbnail": "http://www.burrivery.com/assets/restraurants/",
+        },
+    },
+    {
+        "title": "Буркинг",
+        "subtitle": "$$$, burgers, salads, fastfood",
+        "picture": {
+            "thumbnail": "http://www.burrivery.com/assets/restraurants/",
+        },
+    },
+    {
+        "title": "Му-Му",
+        "subtitle": "$, healthy, russian, soups",
+        "picture": {
+            "thumbnail": "http://www.burrivery.com/assets/restraurants/",
+        },
+    },
+    {
+        "title": "Якитория",
+        "subtitle": "$$, sushi, japanese, soups",
+        "picture": {
+            "thumbnail": "http://www.burrivery.com/assets/restraurants/",
+        },
+    },
+    {
+        "title": "Автосуши",
+        "subtitle": "$$, sushi, pizza",
+        "picture": {
+            "thumbnail": "http://www.burrivery.com/assets/restraurants/",
+        },
+    },
+    {
+        "title": "Steak Home",
+        "subtitle": "$$$$, meat, steak",
+        "picture": {
+            "thumbnail": "http://www.burrivery.com/assets/restraurants/",
+        },
+    },
+    {
+        "title": "Папа Джонс",
+        "subtitle": "$$, pizza, steak",
+        "picture": {
+            "thumbnail": "http://www.burrivery.com/assets/restraurants/",
+        },
+    }
+];
